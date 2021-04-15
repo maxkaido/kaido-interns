64fd8918d5a9e7191bfed7c85d7d225fb8bf16e3
Max Kaido <maximkaido@gmail.com>
2021-04-15 11:47:55 +0200

    init experiments

---
+++ experiments/6-brainjs/build/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A = void 0;
+const A = 'A';
+exports.A = A;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsTUFBTSxDQUFDLEdBQUcsR0FBRyxDQUFBO0FBQ0wsY0FBQyIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IEEgPSAnQSdcbmV4cG9ydCB7QX1cbiJdfQ==
