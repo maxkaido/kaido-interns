a8c5768582d7e748383651b083519c559b9f6507
Max Kaido <maximkaido@gmail.com>
2021-04-18 18:45:35 +0200

    Bot class

--- experiments/8-typescript/src/index.ts
+++ experiments/8-typescript/src/index.ts
@@ -1,10 +1,47 @@
 const A = "A";
 
 class Bot {
-  botToken: string;
-  constructor(botToken: string) {
+  readonly botToken: string;
+
+  bot: any;
+  botInfo: any;
+
+  constructor({
+    botToken,
+    redisUrl,
+    mongoUrl,
+  }: {
+    botToken: string;
+    redisUrl: string;
+    mongoUrl: string;
+  }) {
     this.botToken = botToken;
+  }
+
+  queues: any; // { pulseQueue, notificationQueue }
+  models: any; // { User, Bot }
+  redis: any; // redis client
+  mongo: any; // mongo connection
+  api: any; // express app
+
+  launch() {
+    // connectionFactory(mongoUrl, pool)
+    this.bot.launch();
+    this.api.listen(5000, process.env.HOST);
   }
 }
 
-export { A, Bot };
+class NotificationBot extends Bot {
+  // queues: { notificationQueue }
+  // models : { Notification }
+}
+
+class PulseBot {
+  // queues: { PulseQueue }
+}
+
+class SupportBot {
+  // models: {SupportTicket}
+}
+
+export { A, Bot, NotificationBot, PulseBot };
