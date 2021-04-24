8e31d67075aa228578ac55c59c7402a787317b24
Max Kaido <maximkaido@gmail.com>
2021-04-24 20:00:58 +0200

    test sendCopy not preserving web_page_preview_disable

--- experiments/2-telegraf/src/index.ts
+++ experiments/2-telegraf/src/index.ts
@@ -5,6 +5,13 @@ const Bot = function (this: any, { botToken }: any) {
   this.bot.telegram.getMe().then(console.log);
   this.bot.start((ctx: any) => ctx.reply("telegraf experiment"));
   this.bot.launch();
+  this.bot.command("copy", (ctx: any) => {
+    const { reply_to_message } = ctx.message;
+    // console.log(reply_to_message);
+    if (reply_to_message) {
+      return ctx.telegram.sendCopy(ctx.from.id, reply_to_message);
+    }
+  });
   return this;
 };
 
