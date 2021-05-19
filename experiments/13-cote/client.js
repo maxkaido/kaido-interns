aeb949e7f16397cb8ccb4875e679ee674cc8401e
Max Kaido <maximkaido@gmail.com>
2021-05-19 10:34:46 +0200

    domain 2 ip

--- experiments/13-cote/client.js
+++ experiments/13-cote/client.js
@@ -1,4 +1,4 @@
-const cote = require("cote", { multicast: "dev7.kaido.team" });
+const cote = require("cote", { multicast: "95.216.205.117" });
 const client = new cote.Requester({ name: "Client" });
 
 client.send({ type: "time" }, (time) => {
--- experiments/13-cote/time-service.js
+++ experiments/13-cote/time-service.js
@@ -1,4 +1,4 @@
-const cote = require("cote", { multicast: "dev7.kaido.team" });
+const cote = require("cote", { multicast: "95.216.205.117" });
 const timeService = new cote.Responder({ name: "Time Service" });
 
 timeService.on("time", (req, cb) => {
