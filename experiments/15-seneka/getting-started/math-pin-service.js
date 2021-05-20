ffb8852dd6c7aa61d32f57a4d2a6ab9a8ebb11e9
Max Kaido <maximkaido@gmail.com>
2021-05-20 02:15:34 +0200

    wip

--- experiments/15-seneka/getting-started/math-pin-service.js
+++ experiments/15-seneka/getting-started/math-pin-service.js
@@ -1,17 +1,12 @@
-var Express = require("express");
-var Router = Express.Router;
-var context = new Router();
-
 require("seneca")()
 
   .use("math")
 
   // listen for role:math messages
   // IMPORTANT: must match client
-  .listen({ host: "0.0.0.0", type: "tcp", port: 5001, pin: "role:math" });
-
-var context = new Router();
-var app = Express()
-  .use(require("body-parser").json())
-  .use(context)
-  .listen(5000);
+  .listen({
+    host: "0.0.0.0",
+    type: "tcp",
+    port: process.env.PORT,
+    pin: "role:math",
+  });
